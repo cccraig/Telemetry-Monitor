29422d9a3ceb3ddcee380fa495d6ed7910fccfee
Collin Craige <collincraige@gmail.com>
2017-10-05 17:07:14 -0500

    removed console.log

--- assets/imports.js
+++ assets/imports.js
@@ -1,7 +1,6 @@
 const links = document.querySelectorAll('link[rel="import"]')
 
 Array.prototype.forEach.call(links, function (link) {
-	console.log(link.import)
   let template = link.import.querySelector('.task-template');
   let clone = document.importNode(template.content, true);
   let id = link.dataset.id;
