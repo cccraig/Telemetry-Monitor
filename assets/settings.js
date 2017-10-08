b9b0fa70aa557a34e662d3fc4742232f41c25c0e
Collin Craige <collincraige@gmail.com>
2017-10-08 10:03:49 -0500

    added support for settings persistance and repopulation of the settings form

--- assets/settings.js
+++ assets/settings.js
@@ -1,10 +1,31 @@
-
 $(function() {
+
+	// Populate form with submitted data
+	const Store = require('electron-store');
+	const store = new Store()
+
+	let data = store.get('settings');
+
+	if (data != undefined) {
+		$.each(data, function(name, value) {
+			$("input[name='" + name + "'").val(value)
+		});
+	}
+
+	// Catch form submit
 	$("#settings").submit(function(e) {
 
 		event.preventDefault();
 
-		let data = $("#settings :input").serialize();
+		let values = {};
+
+		let data = $("#settings :input");
+
+		data.each(function() {
+			values[this.name] = $(this).val();
+		});
+
+		store.set('settings', values);
 
 	});
 });
