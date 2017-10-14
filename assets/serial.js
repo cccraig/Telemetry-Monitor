e048677c1ef910105ab25a66a2305e36755938b1
Collin Craige <collincraige@gmail.com>
2017-10-13 20:57:28 -0500

    code streamlining

--- assets/serial.js
+++ assets/serial.js
@@ -1,9 +1,11 @@
 const serialport = require('serialport')
 
-serialport.list((err, ports) => {
+
+// List serial ports and append to dom
+let portList = serialport.list((err, ports) => {
 
     if (err) {
-        document.getElementById('error').textContent = err.message
+        document.getElementById('alerts').textContent = err.message
         return
     } else {
     // document.getElementById('error').textContent = ''
@@ -18,18 +20,22 @@ serialport.list((err, ports) => {
 
         // Append to the dom
         document.getElementById('serialports').appendChild(option);
+    
+    } else {
+
+        // List the ports in a select element
+        ports.forEach(function(port) {
+
+            // Create Option element
+            var option = document.createElement("OPTION")
+
+            // Assign the text for it
+            option.innerHTML = port.comName;
+
+            // Append to the dom
+            document.getElementById('serialports').appendChild(option);
+        });
     }
-
-    // List the ports in a select element
-    options = ports.forEach(function(port) {
-
-        // Create Option element
-        var option = document.createElement("OPTION")
-
-        // Assign the text for it
-        option.innerHTML = port.comName;
-
-        // Append to the dom
-        document.getElementById('serialports').appendChild(option);
-    });
 });
+
+// TODO Persist port and baud settings
