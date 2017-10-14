7b0820b648760783baf508d0d40c8485fc3eb64b
Collin Craige <collincraige@gmail.com>
2017-10-14 16:44:13 -0500

    Fixed naming error

--- assets/settings.js
+++ assets/settings.js
@@ -1,41 +1,39 @@
-$(function() {
-	const Store = require('electron-store');
-	const store = new Store()
-	const userSettings = store.get('settings');
+const Store = require('electron-store');
+const store = new Store()
+const userSettings = store.get('settings');
 
-	if (userSettings != undefined) {
-		$.each(userSettings, function(name, value) {
-			$.each(value, function(n, v) {
-				s = "#settings input[name='" + name + "-" + n + "']";
-				$(s).val(v)
-			});
+if (userSettings != undefined) {
+	$.each(userSettings, function(name, value) {
+		$.each(value, function(n, v) {
+			s = "#settings input[name='" + name + "-" + n + "']";
+			$(s).val(v)
 		});
-	}
+	});
+}
 
-	// Catch form submit
-	$("#settings").submit(function(e) {
+// Catch form submit
+$("#settings").submit(function(e) {
 
-		event.preventDefault();
+	event.preventDefault();
 
-		let values = {
-			battery: {},
-			temperature: {},
-			speed: {}
-		};
+	let values = {
+		battery: {},
+		temperature: {},
+		speed: {}
+	};
 
-		let userSettings = $("#settings :input").not(':button');
+	let userSettings = $("#settings :input").not(':button');
 
-		userSettings.each(function() {
+	userSettings.each(function() {
 
-			let parsed = this.name.split('-');
-			let gauge = parsed[0];
-			let option = parsed[1];
+		let parsed = this.name.split('-');
+		let gauge = parsed[0];
+		let option = parsed[1];
 
-			values[gauge][option] = $(this).val();
-			
-		});
-
-		store.set('settings', values);
+		values[gauge][option] = $(this).val();
 
 	});
+
+	store.set('settings', values);
+
 });
