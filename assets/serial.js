a80b58103d4148f70fc7e06cfedf614d71439da7
Collin Craige <collincraige@gmail.com>
2017-10-15 16:26:10 -0500

    converted to serial class and fixed some errors.

--- assets/serial.js
+++ assets/serial.js
@@ -1,109 +1,117 @@
 const SerialPort = require('serialport')
 const Store = require('electron-store');
 const store = new Store()
-const defaultConnection = store.get('default_serial');
+const defaultPort = store.get('default_serial');
 
-// Update the serial port connection
-var updateSerialPort = function(obj) {
 
-  let portBox = document.getElementById('serialports');
-  let port = portBox.options[portBox.selectedIndex].value;
+class Communication {
 
-  let baudBox = document.getElementById('bauds');
-  let baud = baudBox.options[baudBox.selectedIndex].value;
-
-  let update = {
-    "port":port,
-    "baud":baud,
-    "port_index": portBox.selectedIndex,
-    "baud_index": baudBox.selectedIndex
+  constructor(defaultPort) {
+    this.defaultPort = defaultPort;
   }
 
-  store.set('default_serial', update);
-}
+
+  listPorts(err, ports) {
+    if(err) {Communication.msg({"msg":'two'}); return;};
+
+    if(ports.length === 0) {Communication.populateComList({comName:"No Ports Discovered"}); return;};
+
+    ports.forEach(function(port,key){Communication.populateComList(port, key)});
+  }
 
 
-// Update the baud rate
-var updateBaudRate = function() {
-    console.log("working");
-}
+  setDefaultPort() {
+    // Specify an onchange event for serial port and baud rate
+    document.getElementById('serialports').onchange = Communication.updateSerialPort;
+    document.getElementById('bauds').onchange = Communication.updateSerialPort;
 
+    // Select the default port
+    if(this.defaultPort === undefined) {
+      let portBox = document.getElementById('serialports');
+      let baudBox = document.getElementById('bauds');
+      let com = portBox.options[portBox.selectedIndex].value;
+      let baud = baudBox.options[baudBox.selectedIndex].value;
+      let update = {
+        "port":com,
+        "baud":baud,
+        "port_index": portBox.selectedIndex,
+        "baud_index": baudBox.selectedIndex
+      };
 
-// Do something when data arrives
-var parseIncomingData = function(data) {
-    console.log(data);
-}
+      this.defaultPort = update;
 
-
-var displayError = function(err) {
-    document.getElementById('error_msg').value = err.message;
-    console.log(err);
-}
-
-// List serial ports and append to dom
-SerialPort.list((err, ports) => {
-
-    if (err) {
-        document.getElementById('error_msg').value = err.message; return;
-    }
-
-    if (ports.length === 0) {
-        // Create Option element
-        var option = document.createElement("OPTION")
-
-        // Assign the text
-        option.innerHTML = "No ports discovered";
-
-        // Assign a value
-        option.value = 0;
-
-        // Append to the dom
-        document.getElementById('serialports').appendChild(option);
+      store.set('default_serial', update);
 
     } else {
+      let com_index = this.defaultPort['port_index'];
+      let baud_index = this.defaultPort['baud_index'];
 
-        // List the ports in a select element
-        ports.forEach(function(port, key) {
+      document.getElementById('serialports').selectedIndex = com_index;
+      document.getElementById('bauds').selectedIndex = baud_index;
+    }
+  }
 
-            // Create Option element
-            var option = document.createElement("OPTION")
 
-            // Assign the text
-            option.innerHTML = port.comName;
+  openDefaultPort() {
+    let com = this.defaultPort["port"];
+    let baud = this.defaultPort["baud"];
+    let parser = new SerialPort.parsers.Readline();
 
-            // Assign a value
-            option.value = port.comName;
+    this.port = new SerialPort(com, {"baudRate":parseInt(baud)}, Communication.msg);
+    this.port.pipe(new SerialPort.parsers.Readline());
+    parser.on('data', console.log);
+    this.port.write('respond\n');
+  }
 
-            // Append to the dom
-            document.getElementById('serialports').appendChild(option);
-        });
 
-        // Specify an onchange event for serial port
-        document.getElementById('serialports').onchange = updateSerialPort;
+  static updateSerialPort() {
+    let portBox = document.getElementById('serialports');
+    let port = portBox.options[portBox.selectedIndex].value;
 
-        // Specify an onchange event for baud rate
-        document.getElementById('bauds').onchange = updateSerialPort;
+    let baudBox = document.getElementById('bauds');
+    let baud = baudBox.options[baudBox.selectedIndex].value;
+    console.log(portBox.selectedIndex)
+    let update = {
+      "port":port,
+      "baud":baud,
+      "port_index": portBox.selectedIndex,
+      "baud_index": baudBox.selectedIndex
     }
 
-}).then(function() {
+    store.set('default_serial', update);
+  }
 
-    // Open the default port or create one
-    if(defaultConnection === undefined) {
 
-        let port = document.getElementById('serialports').options[0].value;
-        var dps = {"port": port, "baud":9600, "port_index":0, "baud_index":0}
-        store.set('default_serial', dps);
-
-    } else {
-
-        var dps = store.get('default_serial');
-        document.getElementById('serialports').options.selectedIndex = dps["port_index"]
-        document.getElementById('bauds').options.selectedIndex = dps["baud_index"]
-
+  static msg(err) {
+    if(err.msg !== undefined) {
+      err = err.msg;
     }
+    document.getElementById('error_msg').value = err;
+    console.log(err)
+  }
 
-    const activePort = new SerialPort(dps["port"], baudRate=dps["baud"], displayError);
-    const Readline = SerialPort.parsers.Readline;
-    const parser = activePort.pipe(new Readline());
-    parser.on('data', parseIncomingData);
-});
+
+  static populateComList(port, val=0) {
+    // Create Option element
+    let option = document.createElement("OPTION")
+
+    // Assign the text
+    option.innerHTML = port.comName;
+
+    // Assign a value
+    option.value = port.comName;
+
+    // Append to the dom
+    document.getElementById('serialports').appendChild(option);
+  }
+}
+
+
+let Radio = new Communication(defaultPort);
+
+SerialPort.list(Radio.listPorts)
+  .then(function() {
+    Radio.setDefaultPort();
+  }).then(function() {
+    Radio.openDefaultPort();
+  });
