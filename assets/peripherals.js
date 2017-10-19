dae8fad44366f2345e231e8a999a406967b89582
Collin Craige <collincraige@gmail.com>
2017-10-19 16:06:38 -0500

    Animated the plot for testing

--- assets/peripherals.js
+++ assets/peripherals.js
@@ -3,11 +3,22 @@ const Legend = require('chartist-plugin-legend');
 
 var menu = document.querySelectorAll("[data-menu='peripherals']")[0];
 
+var x_labels = [-30,-29,-28,-27,-26,-25,-24,-23,-22,-21,-20,-19,-18,-17,-16,
+  -15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0];
+
+var series = [
+  [5, 5, 10, 8, 7, 5, 4, null, null, null, 10, 10, 7, 8, 6, 9],
+  [10, 15, null, 12, null, 10, 12, 15, null, null, 12, null, 14, null, null, null],
+  [null, null, null, null, 3, 4, 1, 3, 4,  6,  7,  9, 5, null, null, null],
+]
+
+chart = drawCharts(series, x_labels)
+
 $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-  drawCharts();
+  chart.update({"labels":x_labels, "series":series})
 });
 
-function drawCharts() {
+function drawCharts(data, x_labels) {
   let options = {
     onlyInteger: true,
     fullWidth: true,
@@ -21,19 +32,33 @@ function drawCharts() {
     low: 0,
     plugins: [
       new Legend({
-        legendNames: ['Series1', 'Series2', 'Series3', 'Series4'],
+        legendNames: ['Series1', 'Series2', 'Series3'],
       })
     ]
   };
 
-  let x_labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-  let series1 = [5, 5, 10, 8, 7, 5, 4, null, null, null, 10, 10, 7, 8, 6, 9];
-  let series2 = [10, 15, null, 12, null, 10, 12, 15, null, null, 12, null, 14, null, null, null];
-  let series3 = [null, null, null, null, 3, 4, 1, 3, 4,  6,  7,  9, 5, null, null, null];
-  let series4 = [{x:3, y: 3},{x: 4, y: 3}, {x: 5, y: undefined}, {x: 6, y: 4}, {x: 7, y: null}, {x: 8, y: 4}, {x: 9, y: 4}];
-
   var chart = new Chartist.Line('#chart1', {
     labels: x_labels,
-    series: [series1, series2, series3, series4],
+    series: data,
   }, options);
+
+  return chart;
 }
+
+
+
+setInterval(function() {
+  let n = series[0].length;
+
+  for (var i = 0; i < series.length; i++) {
+    if(n == 30) {
+      series[i].shift();
+    }
+    series[i].push(Math.random() * 10);
+  }
+
+  chart.update({
+    "labels":x_labels,
+    "series":series
+    });
+}, 1000);
