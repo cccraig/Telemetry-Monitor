fcd13c64013645dde7cb7e1fe71e213d16347b64
Collin Craige <collincraig@gmail.com>
2017-10-29 14:42:07 -0500

    JS lint fixes

--- assets/settings.js
+++ assets/settings.js
@@ -1,57 +1,54 @@
 const Store = require('electron-store');
 
 class Settings {
-
   constructor() {
     this.store = new Store();
     this.settings = this.store.get('settings');
-		this.populate();
-		this.catchFormSubmit();
+    this.populate();
+    this.catchFormSubmit();
   }
 
-
   populate() {
-    if(this.settings !== undefined) {
-  		for (const [name, value] of Object.entries(this.settings)) {
-  		  for (const [n, v] of Object.entries(value)) {
-  		    const s = "input[name='" + name + "-" + n + "']";
-  		    document.querySelector(s).value = v;
-  		  }
-  		}
+    if (this.settings !== undefined) {
+      for (const [name, value] of Object.entries(this.settings)) {
+        for (const [n, v] of Object.entries(value)) {
+          const s = "input[name='" + name + '-' + n + '"]"';
+          document.querySelector(s).value = v;
+        }
+      }
     }
   }
 
+  catchFormSubmit() {
+    var settingsForm = document.querySelector('#form_settings');
 
-	catchFormSubmit() {
+    settingsForm.addEventListener('submit', function (e) {
 
-		var settings_form = document.querySelector("#form_settings");
+      event.preventDefault();
 
-		settings_form.addEventListener("submit", function(e) {
+      let data = {};
 
-			event.preventDefault();
+      let settings = document.querySelectorAll('#form_settings input');
 
-			let data = {}
+      const l = settings.length;
 
-			let settings = document.querySelectorAll('#form_settings input');
+      for (var i = 0; i < l; i++) {
+        let inputName = settings[i].name;
+        let parsed = inputName.split('-');
+        const name = parsed[0];
+        const option = parsed[1];
+        const value = settings[i].value;
 
-			const l = settings.length;
+        if (name in data) {
+          data[name][option] = value;
+        } else {
+          data[name] = {};
+        }
+      }
 
-			for (var i = 0; i < l; i++) {
-				let input_name = settings[i].name;
-		    let parsed = input_name.split('-');
-		    const name = parsed[0];
-		    const option = parsed[1];
-				const value = settings[i].value;
-
-				if (name in data) {
-					data[name][option] = value;
-				} else {
-					data[name] = {}
-				}
-			}
-			store.set('settings', data);
-		});
-	}
+      store.set('settings', data);
+    });
+  }
 }
 
 module.exports = Settings;
